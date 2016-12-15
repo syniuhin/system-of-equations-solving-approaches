31ddbc25dcbb2ebfc2101500add54d60bc6ab94a
LimeTheCoder <limethecoder@gmail.com>
2016-12-15 03:29:02 +0200

    Add determinant computing

--- methods/terry/thomas_algorithm.py
+++ methods/terry/thomas_algorithm.py
@@ -1,40 +1,45 @@
 A = [
-    [1, 2, 0, 0],
-    [2, -1, 1, 0],
-    [0, 1, -1, 1],
-    [0, 0, 1, 1]
+    [5, -1, 0, 0],
+    [2, 4.6, -1, 0],
+    [0, 2, 3.6, -0.8],
+    [0, 0, 3, 4.4]
 ]
 
 y = [
-    [5],
-    [3],
-    [3],
-    [7]
+    [2],
+    [3.3],
+    [2.6],
+    [7.2]
 ]
 
 
 def solve(A, y):
     n = len(A)
 
-    sigma = -1. * A[0][1] / A[0][0]
-    lamb = 1. * y[0][0] / A[0][0]
-    coeff = [(sigma, lamb)]
+    coeff =[]
+    det = 1
+    sigma = 0
+    lamb = 0
 
-    for i in xrange(1, n - 1):
-        b = A[i][i - 1]
-        c = A[i][i]
-        d = A[i][i + 1]
+    for i in xrange(n):
+        b = (1. * A[i][i - 1]) if i - 1 >= 0 else 0.
+        c = 1. * A[i][i]
+        d = (1. * A[i][i + 1]) if i + 1 != n else 0.
+        det *= (c + b * sigma)
         sigma, lamb = -d / (c + b * sigma), (y[i][0] - b * lamb) / (c + b * sigma)
         coeff.append((sigma, lamb))
 
-    xn = (y[n - 1][0] - A[n - 1][n - 2] * lamb) / \
-                       (A[n - 1][n - 1] + A[n - 1][n - 2] * sigma)
+    xn = lamb
     roots = [xn]
+
     for i in xrange(n - 2, -1, -1):
         sigma, lamb = coeff[i]
         xn = sigma * xn + lamb
         roots.insert(0, xn)
 
-    return roots
+    return roots, det
 
-print solve(A, y)
+if __name__ == '__main__':
+    x, det = solve(A, y)
+    print x
+    print det
