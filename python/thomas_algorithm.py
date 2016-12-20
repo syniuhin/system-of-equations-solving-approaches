2d31905bb8e9cfed3fff4265d4a6a320574650f7
LimeTheCoder <limethecoder@gmail.com>
2016-12-20 18:59:01 +0200

    Add correctness check

--- python/thomas_algorithm.py
+++ python/thomas_algorithm.py
@@ -1,10 +1,11 @@
 import time
 
-from matrix_tool import print_matrix, print_vector
+from matrix_tool import print_vector
 from check import check_roots
 
 
 def solve(A, y):
+
   n = len(A)
 
   coeff = []
@@ -29,9 +30,31 @@ def solve(A, y):
   return roots, det
 
 
+def is_stable(A):
+    n = len(A)
+
+    is_strong_eq = False
+
+    for i in xrange(n):
+        b = A[i][i - 1] if i - 1 >= 0 else 0
+        c = A[i][i]
+        d = A[i][i + 1] if i + 1 != n else 0
+
+        if abs(c) < abs(b) + abs(d):
+            return False
+
+        if abs(c) > abs(b) + abs(d):
+            is_strong_eq = True
+
+    return is_strong_eq
+
+
 def main():
   from constants import A3_21 as A, b3_21 as b
   start_time = time.time()
+  print ("Correctness and stability check: {}"
+         .format(is_stable(A)))
+  check_time = time.time()
   x, det = solve(A, b)
   solve_time = time.time()
   print("Roots:")
@@ -41,7 +64,9 @@ def main():
   print("Determinant: {}".format(det))
 
   print("Time elapsed: ")
-  print("\tSolution:\t{} ms".format((solve_time - start_time) * 1000))
+  print("\tCheck: \t\t{} ms\n\tSolution:\t{} ms\n-------------\n\tOverall:\t{} ms"
+        .format((check_time - start_time) * 1000, (solve_time - check_time) * 1000,
+                (solve_time - start_time) * 1000))
 
 
 if __name__ == '__main__':
